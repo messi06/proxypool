a0f639b8f4861fd0b34751891a4abcbe8c78e056
Shadowx <samib06@hotmail.fr>
2020-12-06 04:08:20 +0800

    Update check.go

--- pkg/proxy/check.go
+++ pkg/proxy/check.go
@@ -12,7 +12,7 @@ import (
 	"github.com/Dreamacro/clash/adapters/outbound"
 )
 
-const defaultURLTestTimeout = time.Second * 2
+const defaultURLTestTimeout = time.Second * 8
 
 func testDelay(p Proxy) (delay uint16, err error) {
 	pmap := make(map[string]interface{})
